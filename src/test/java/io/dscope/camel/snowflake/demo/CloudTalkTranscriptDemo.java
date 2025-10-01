0ce775161389765199ef290dd58fe0ea7a1bdafa
Roman Dobrik <roman.dobrik@exilor.com>
2025-10-01 12:42:49 -0700

    removed unused file

--- src/test/java/io/dscope/camel/snowflake/demo/CloudTalkTranscriptDemo.java
+++
@@ -1,4 +0,0 @@
-// Deprecated placeholder. This file previously contained a demo class with a CloudTalk-related name.
-// The canonical demo is now `SampleQueryDemo` in the same package:
-//   - src/test/java/io/dscope/camel/snowflake/demo/SampleQueryDemo.java
-// This file is intentionally left without any types to avoid compilation conflicts and will be removed.
